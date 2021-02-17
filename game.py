6cfc5768ac7d8888983f6e262caa4b3b5b82a63b
olya_bo <mycleverowl@gmail.com>
2021-02-18 00:37:54 +0200

    Version 1.2

--- game.py
+++ game.py
@@ -1,22 +1,31 @@
 from exceptions import GameOver, EnemyDown
 from models import Enemy, Player
+from settings import OPTIONS, START, SHOW_SCORES, EXIT, HELP
 
 
 def play():
     name = input('Enter your name: ')
-    option = input('Enter start if you are not a loser')
-    if option == 'start':
-        player = Player(name)
-        level = 1
-        enemy = Enemy(level)
-        while True:
-            try:
-                player.attack(enemy)
-                player.defence(enemy)
-            except EnemyDown:
-                level += 1
-                enemy = Enemy(level)
-                player.score += 5
+    while True:
+        option = input('Enter start if you are not a loser: ')
+        if option == START:
+            player = Player(name)
+            level = 1
+            enemy = Enemy(level)
+            while True:
+                try:
+                    player.attack(enemy)
+                    player.defence(enemy)
+                except EnemyDown:
+                    level += 1
+                    enemy = Enemy(level)
+                    player.score += 5
+        elif option == SHOW_SCORES:
+            pass
+        elif option == EXIT:
+            raise KeyboardInterrupt
+        elif option == HELP:
+            print('Option list:')
+            print("\n".join(OPTIONS))
 
 
 if __name__ == '__main__':
@@ -24,6 +33,7 @@ if __name__ == '__main__':
         play()
     except GameOver as e:
         print(e)
+        print(f"You loose! Your score is {e.score}.")
         e.write_score()
     except KeyboardInterrupt:
         pass
--- settings.py
+++ settings.py
@@ -1,2 +1,8 @@
 PLAYERS_LIVES = 3
 START_PLAYERS_SCORE = 0
+
+START = "start"
+SHOW_SCORES = "show scores"
+EXIT = "exit"
+HELP = "help"
+OPTIONS = [START, SHOW_SCORES, EXIT, HELP]
